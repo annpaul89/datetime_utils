baad2bfcdaa12cdcca371b46b882aca4ad42bd5c
Ann Paul <annmollyp@gmail.com>
2016-04-30 21:42:48 -0700

    Add version file

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,22 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import sys
 import os
+import re
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
